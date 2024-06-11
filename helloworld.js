be8489ff22aaef247b4ede6b97d60b00b5375334
aaronblac <32806041+aaronblac@users.noreply.github.com>
2024-06-10 22:19:29 -0700

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,7 @@ var http = require("http")
 
 http.createServer(function (request, response) {
 
-   // Send the HTTP heade
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
